e699ef0380a8bdef039212383b50febdb3630645
Caiua França <caiuagomes@gmail.com>
2014-02-23 12:11:37 -0300

    primeiro comit

--- app.py
+++ app.py
@@ -1,28 +1,42 @@
 from flask import Flask, render_template, flash, url_for, redirect, request
+import os
+import cgi
 
 app = Flask(__name__)
+
 app.config.update({
     'SECRET_KEY': 'Evolux <3 Python',
     'DEBUG': True
 })
 
+@app.route("/<nome>")
+def hello(nome):
+    return "Bar Cadastrado com Sucesso! <br/> %s" % nome
+
 @app.route("/", methods=['GET', 'POST'])
 def cadastro():
-    if request.methods == 'POST':
-	 	if request.form.get('nome'):
-			return redirect(
-				url_for(
-					'Bar Cadastrado!', nome=request.form.get('nome')  
-				)
-			)
-		else:
-			flash('Insira os Dados Para Cadastro')
-
+    if request.method == 'POST':
+        if request.form.get('nome'):
+            salvar()                                       
+        else:
+            flash('Insira os Dados para Cadastrar')
     return render_template("index.html")
 
-@app.route("/")
-def hello():
-    return "Hello World!"
-
+def salvar():
+    with open("db/db.txt", "a") as text_file:
+        text_file.write('\n')
+        text_file.write('Cadastro do Bar')
+        text_file.write('\n')
+        text_file.write(request.form.get('nome'))
+        text_file.write('\n')
+        text_file.write(request.form.get('endereco'))
+        text_file.write('\n')
+        text_file.write(request.form.get('funcionamento'))
+        text_file.write('\n')
+        text_file.write(request.form.get('especialidade'))
+        text_file.write('\n')
+        text_file.write('----------------------------------')
+        
+        
 if __name__ == "__main__":
     app.run(debug=True)
